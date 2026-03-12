f7000bbb9d0f3adcd70558467f7c3b43b0080ce4
Peter Lambert <peter.lambert@brokenbayassociates.com.au>
2026-03-12 19:04:49 +1100

    chore: add Dockerfile and docker-compose for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/aemo': {
-        target: 'https://nemweb.com.au',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/aemo/, ''),
-      }
-    }
-  }
 })
